2963465a8ec2e87a87f4ac47e7f627a1d33794dd
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-18 02:37:34 +0800

    Update home and error views to extend the layout
    - Updated home/index.php and _error.php to use the new layout system.
    - Defined specific sections for each view, including title and content.

--- app/views/_error.php
+++ app/views/_error.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Error | ITPEC Exam Review</title>
-	</head>
-	<body>
-		<h1>An Error Occurred</h1>
-		<p> <?= htmlspecialchars($message) ?> </p>
-	</body>
-</html>
+<?php
+$title = "Error | ITPEC Exam Review"; ?>
+
+<h1>An Error Occurred</h1>
+<p><?= htmlspecialchars($message) ?></p>
