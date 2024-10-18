0ec973fbdba29f08759c29638f759f5fd4d30307
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-19 01:40:27 +0800

    Create placeholder view for displaying exam set page
    - Added a view for showing the details of a specific exam set within an exam.
    - Dynamically sets the page title and shows the exam and exam set names.

---
+++ app/views/exam/examset.php
@@ -0,0 +1,7 @@
+<?php
+$title = htmlspecialchars($examset_name) . " | ITPEC Exam Review"; ?>
+
+<h1>
+    <?= htmlspecialchars($examset_name) ?>
+</h1>
+<p>This is the page for the <?= htmlspecialchars($examset_name) ?> exam set under <?= htmlspecialchars($exam_name) ?>.</p>
