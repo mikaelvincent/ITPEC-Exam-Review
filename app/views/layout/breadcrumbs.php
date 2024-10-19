41d0565f1dd5f60a850c7a94b9d86546c3b50166
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-20 03:26:23 +0800

    Removed nav tags
    - Removed nav tags to be consistent with design

--- app/views/layout/breadcrumbs.php
+++ app/views/layout/breadcrumbs.php
@@ -1,23 +1,21 @@
 <?php if (!empty($breadcrumbs)): ?>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb mb-5">
-            <?php foreach ($breadcrumbs as $index => $breadcrumb): ?>
-                <?php if ($index < count($breadcrumbs) - 1): ?>
-                    <li class="breadcrumb-item">
-                        <a class="text-decoration-none" href="<?= htmlspecialchars($breadcrumb["path"]) ?>">
-                            <span>
-                                <?= htmlspecialchars($breadcrumb["title"]) ?>
-                            </span>
-                        </a>
-                    </li>
-                <?php else: ?>
-                    <li class="breadcrumb-item active" aria-current="page">
+    <ol class="breadcrumb mb-5">
+        <?php foreach ($breadcrumbs as $index => $breadcrumb): ?>
+            <?php if ($index < count($breadcrumbs) - 1): ?>
+                <li class="breadcrumb-item">
+                    <a class="text-decoration-none" href="<?= htmlspecialchars($breadcrumb["path"]) ?>">
                         <span>
                             <?= htmlspecialchars($breadcrumb["title"]) ?>
                         </span>
-                    </li>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </ol>
-    </nav>
+                    </a>
+                </li>
+            <?php else: ?>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <span>
+                        <?= htmlspecialchars($breadcrumb["title"]) ?>
+                    </span>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ol>
 <?php endif; ?>
