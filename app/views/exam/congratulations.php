92a3bffb7ea9859eb17f47f7f1e506dec581a61b
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-20 03:25:06 +0800

    Add static congratulations page view
    - Implement the static view for the congratulations page with placeholder content within the exam section.

---
+++ app/views/exam/congratulations.php
@@ -0,0 +1,25 @@
+<?php
+$title = "Congratulations | " . htmlspecialchars($examset_name) . " | ITPEC Exam Review"; ?>
+
+<div class="row mb-5">
+    <div class="col-md-10 col-xl-8 d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center">
+        <div>
+            <h1 class="text-uppercase fw-bold text-center mb-4">All Questions Completed</h1>
+            <div class="lead">
+                <p>Great job! You've worked through all the available FE Exam questions in our review system.</p>
+                <p>Here’s what you can do next:</p>
+                <ul>
+                    <li>
+                        <span class="fw-bold">Reset Your Progress:</span> If you'd like to continue practicing, consider resetting your progress. This will allow you to go over the questions again, reinforcing key concepts and improving your problem-solving approach
+                    </li>
+                    <li>
+                        <span class="fw-bold">Check for New Questions:</span> While you’ve completed all the available question sets, we update our database periodically with new content. Be sure to check back regularly for fresh questions to keep sharpening your skills.
+                    </li>
+                </ul>
+                <p>Thank you for using our review platform to prepare for the ITPEC FE Exam. You've put in the work, and your effort will pay off. Keep practicing, and good luck!</p>
+                <p>—&nbsp; <a class="fw-bold text-decoration-none" href="#">The ITPEC Exam Review Team</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
