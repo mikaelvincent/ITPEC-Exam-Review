6cb693191d558001d5ef883dd51f932357cecd0e
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-20 03:27:36 +0800

    Add static question view
    - Implement the static view for the question page with placeholder content.

--- app/views/exam/question.php
+++ app/views/exam/question.php
@@ -1,5 +1,76 @@
 <?php
 $title = "Q" . htmlspecialchars($question_number) . " | ITPEC Exam Review"; ?>
 
-<h1>Question <?= htmlspecialchars($question_number) ?></h1>
-<p>This is the page for Question <?= htmlspecialchars($question_number) ?> in the <?= htmlspecialchars($examset_name) ?> exam set of <?= htmlspecialchars($exam_name) ?>.</p>
+<div class="row">
+    <div class="col-12 col-lg-8 order-lg-last">
+        <div class="row mb-5">
+            <div class="col">
+                <img class="img-fluid" src="assets/img/2007Apr_FE_AM_Q1.png">
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-4">
+        <div class="row gy-3 row-cols-2 row-cols-xl-1 mb-5">
+            <div class="col">
+                <button class="btn btn-outline-primary btn-lg w-100 py-4" type="button">A</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-outline-primary btn-lg active w-100 py-4" type="button">B</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-danger btn-lg disabled w-100 py-4" type="button" disabled="">C</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-success btn-lg disabled active w-100 py-4" type="button" disabled="">D</button>
+            </div>
+        </div>
+        <hr class="mb-5">
+        <div class="row gy-3 row-cols-1 mb-5">
+            <div class="col">
+                <button class="btn btn-success btn-lg w-100" type="button">Submit</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-success btn-lg disabled w-100" type="button" disabled="">Submit</button>
+            </div>
+            <div class="col">
+                <div class="modal fade" role="dialog" tabindex="-1" id="explanations">
+                    <div class="modal-dialog modal-fullscreen" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title fw-bold">FE Exam / 2007 April - AM / Q1</h4>
+                                <button class="btn-close" type="button" aria-label="Close" data-bs-dismiss="modal"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>This is a placeholder for the AI-generated explanation.</p>
+                            </div>
+                            <div class="modal-footer text-bg-dark d-flex justify-content-center">
+                                <div class="me-sm-auto">
+                                    <button class="btn btn-link btn-sm" type="button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-left fs-3">
+                                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path>
+                                        </svg>
+                                    </button>
+                                    <span class="fs-5">3 / 3</span>
+                                    <button class="btn btn-link btn-sm disabled" type="button" disabled="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-right fs-3">
+                                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <button class="btn btn-primary btn-lg" type="button">Generate New Explanation</button>
+                                <button class="btn btn-secondary btn-lg d-none d-sm-flex" type="button" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn btn-outline-primary btn-lg w-100" type="button" data-bs-target="#explanations" data-bs-toggle="modal">See Explanation</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-primary btn-lg w-100" type="button">Next Question</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-outline-success btn-lg w-100" type="button">Next Question</button>
+            </div>
+        </div>
+    </div>
+</div>
