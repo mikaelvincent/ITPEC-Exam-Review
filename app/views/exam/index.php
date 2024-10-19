38784b44e949975035731a1f8e8abb77d5866242
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-20 03:31:45 +0800

    Add static exam index view
    - Implement the static view for the exam index page with placeholder content.

--- app/views/exam/index.php
+++ app/views/exam/index.php
@@ -1,7 +1,37 @@
 <?php
 $title = htmlspecialchars($exam_name) . " | ITPEC Exam Review"; ?>
 
-<h1>
-    <?= htmlspecialchars($exam_name) ?>
-</h1>
-<p>This is the page for the <?= htmlspecialchars($exam_name) ?> exam.</p>
+<div class="row gy-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xxl-5 mb-5">
+					<div class="col">
+						<a class="btn btn-secondary btn-lg h-100 w-100" role="button" href="#">2008 April - AM</a>
+					</div>
+					<div class="col">
+						<a class="btn btn-success btn-lg h-100 w-100" role="button" href="#">2007 October - AM</a>
+					</div>
+					<div class="col">
+						<a class="btn btn-secondary btn-lg disabled h-100 w-100" role="button" href="#">2007 April - AM</a>
+					</div>
+				</div>
+				<div class="row row-cols-1 mb-5">
+					<div class="col">
+						<button class="btn btn-outline-warning btn-lg w-100" type="button" data-bs-target="#reset-progress-confirm" data-bs-toggle="modal">Reset Progress for FE Exam</button>
+						<div class="modal fade" role="dialog" tabindex="-1" id="reset-progress-confirm">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h4 class="modal-title fw-bold">Confirm Progress Reset</h4>
+										<button class="btn-close" type="button" aria-label="Close" data-bs-dismiss="modal"></button>
+									</div>
+									<div class="modal-body">
+										<p>Are you sure you want to reset your progress for FE Exam?</p>
+										<p>This action is irreversible and cannot be undone.</p>
+									</div>
+									<div class="modal-footer">
+										<button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Cancel</button>
+										<button class="btn btn-danger" type="button">Reset All Progress</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
